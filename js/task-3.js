244129fb5c7b53033690922af607cf1bcf30cfbc
Nejdan4k <a.stryzhavchuk@gmail.com>
2025-07-19 00:00:09 -0400

    'update'

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-textInput.addEventListener('input', event => {
-  let a = event.currentTarget.value.trim();
-  output.textContent = a === '' ? 'Anonymous' : a;
-});
